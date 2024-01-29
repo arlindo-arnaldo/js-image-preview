0d42124fd666ddd543cea7becc6c54e0c25e5d25
Arlindo Arnaldo <arlindoarnaldocassuende@gmail.com>
2024-01-29 15:58:52 -0300

    Finalizando

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function previewImage(){
+function previewImage() {
     //getting the uploaded file details
     var img = document.querySelector('input[name=image]').files[0];
     //getting the preview component id
@@ -7,23 +7,22 @@ function previewImage(){
     //Creating a new instance of the FileReader class
     var reader = new FileReader();
 
-    //when the uplodaded image has been finnal read, set the image to be diplayed
-    reader.onloadend = function() {
+    //when the uploaded image has been finally read, set the image to be diplayed
+    reader.onloadend = function () {
         preview.src = reader.result;
     }
 
     //if there is a form request with the key 'image'
-    if(image){
+    if (image) {
         //read the referenced key value from the URL
         reader.readAsDataURL(img);
         //Once preview image has been clicked, open the file dioalog
-        preview.onclick = function(){
+        preview.onclick = function () {
             document.querySelector("#image").click();
         }
         //if there is not a form request with the key 'image'
-    }else{
+    } else {
         //Set a blank source for the image preview
         preview.src = ''
-    }   
+    }
 }
-
